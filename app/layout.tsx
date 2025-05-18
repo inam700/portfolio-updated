7e1025c129831e64674ab58ebc1eba3f6e664477
inam700 <inaam040@gmail.com>
2025-05-19 00:17:08 +0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Inam Ullah | Portfolio",
   description: "Full Stack Developer",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
